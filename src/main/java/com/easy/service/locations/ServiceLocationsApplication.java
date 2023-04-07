17bf652040f560868f1bf6b546b078c726ea7108
Cristian Villanova <cristiandavidv@hotmail.com>
2023-04-07 15:17:54 -0500

    First commit

---
+++ src/main/java/com/easy/service/locations/ServiceLocationsApplication.java
@@ -0,0 +1,13 @@
+package com.easy.service.locations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLocationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceLocationsApplication.class, args);
+	}
+
+}
